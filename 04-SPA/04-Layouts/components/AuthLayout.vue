45f308ad8341b82bae50ba7cf5ae5f7ae493a908
Alexander Uskov <alex.uskov92@gmail.com>
2020-11-17 03:15:39 +0600

    add solution for 04-SPA 04-Layouts

--- 04-SPA/04-Layouts/components/AuthLayout.vue
+++ 04-SPA/04-Layouts/components/AuthLayout.vue
@@ -1,9 +1,47 @@
-<template></template>
+<template>
+  <div class="page page_onboarding">
+    <div class="container">
+      <h1 class="page__title text-center">
+        {{ title }}
+      </h1>
+      <slot></slot>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
   name: 'AuthLayout',
+
+  props: {
+    title: String,
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.page.page_onboarding {
+  max-width: 374px;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.page__title {
+  font-family: Roboto, sans-serif;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 37px;
+  color: var(--body-color);
+  margin: 0 0 32px;
+}
+
+.text-center {
+  text-align: center;
+}
+
+.container {
+  max-width: 1008px;
+  width: 100%;
+  margin: 0 auto;
+}
+</style>
--- 04-SPA/04-Layouts/components/BaseLayout.vue
+++ 04-SPA/04-Layouts/components/BaseLayout.vue
@@ -1,13 +1,35 @@
-<template></template>
+<template>
+  <div class="wrapper bg-grey">
+    <the-header></the-header>
+    <main>
+      <router-view />
+    </main>
+    <the-footer></the-footer>
+  </div>
+</template>
 
 <script>
+import TheHeader from './TheHeader';
+import TheFooter from './TheFooter';
 export default {
   name: 'BaseLayout',
+
+  components: { TheFooter, TheHeader },
 };
 </script>
 
 <style scoped>
 main {
   flex: 1 0 auto;
+}
+
+.wrapper {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.bg-grey {
+  background-color: var(--grey-light);
 }
 </style>
