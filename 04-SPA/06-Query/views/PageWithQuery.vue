ef785bd4f7b59da8e81dc64bea7bd62fb9e84284
Alexander Uskov <alex.uskov92@gmail.com>
2020-11-17 22:00:30 +0600

    add solution for 04-SPA 06-Query

--- 04-SPA/06-Query/views/PageWithQuery.vue
+++ 04-SPA/06-Query/views/PageWithQuery.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
-    <meetups-view />
+    <meetups-view
+      :view.sync="view"
+      :date.sync="date"
+      :participation.sync="participation"
+      :search.sync="search"
+    />
   </div>
 </template>
 
@@ -10,6 +15,61 @@ import MeetupsView from '../components/MeetupsView';
 export default {
   name: 'PageWithQuery',
   components: { MeetupsView },
+
+  data() {
+    return {
+      view: 'list',
+      date: 'all',
+      participation: 'all',
+      search: '',
+    };
+  },
+
+  created() {
+    this.updateParamsFromQuery(this.$route.query);
+  },
+
+  computed: {
+    query() {
+      return {
+        ...(this.view !== 'list' && { view: this.view }),
+        ...(this.date !== 'all' && { date: this.date }),
+        ...(this.participation !== 'all' && {
+          participation: this.participation,
+        }),
+        ...(this.search !== '' && { search: this.search }),
+      };
+    },
+  },
+
+  watch: {
+    query(val) {
+      if (this.isSamePath(this.$route.query)) {
+        return;
+      }
+      this.$router.push({ path: '/', query: val });
+    },
+    $route(to) {
+      this.updateParamsFromQuery(to.query);
+    },
+  },
+
+  methods: {
+    isSamePath(query) {
+      return (
+        this.view === query.view &&
+        this.date === query.date &&
+        this.participation === query.participation &&
+        this.search === query.search
+      );
+    },
+    updateParamsFromQuery(query) {
+      this.view = query.view ? query.view : 'list';
+      this.date = query.date ? query.date : 'all';
+      this.participation = query.participation ? query.participation : 'all';
+      this.search = query.search ? query.search : '';
+    },
+  },
 };
 </script>
 
