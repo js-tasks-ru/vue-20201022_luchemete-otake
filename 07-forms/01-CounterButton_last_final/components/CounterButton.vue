60148b5bbe74a2e5bac2d25a45cd8a9214aa32b5
Alexander Uskov <alex.uskov92@gmail.com>
2020-11-19 22:54:03 +0600

    add solution for 07-forms 01-CounterButton_last_final

--- 07-forms/01-CounterButton_last_final/components/CounterButton.vue
+++ 07-forms/01-CounterButton_last_final/components/CounterButton.vue
@@ -1,9 +1,44 @@
 <template>
-  <button></button>
+  <button type="button" @click="increase">{{ innerCount }}</button>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'CounterButton',
+
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  data() {
+    return {
+      innerCount: this.count,
+    };
+  },
+
+  watch: {
+    count(val) {
+      if (val) {
+        this.innerCount = val;
+      }
+    },
+  },
+
+  methods: {
+    increase() {
+      this.innerCount += 1;
+      this.$emit('increment', this.innerCount);
+    },
+  },
+};
 </script>
 
 <style>
